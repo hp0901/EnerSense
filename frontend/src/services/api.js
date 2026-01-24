9a01cd6246f1835e8bb41ac38dac46c01443f7e3
hp0901 <hp98570018@gmail.com>
2026-01-24 20:43:44 +0530

    Aws 2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,8 +7,7 @@ const BASE_URL = "https://enersense.duckdns.org/api/v1";
 export const userProfile =  {
   SENDOTP_API: BASE_URL + "/auth/sendotp",
   SIGNUP_API: BASE_URL + "/auth/signup",
-  LOGIN_API: BASE_URL + "/auth/login",
+  LOGIN_API:  "https://enersense.duckdns.org/api/v1/auth/login",
   RESETPASSTOKEN_API: BASE_URL + "/auth/reset-password-token",
   RESETPASSWORD_API: BASE_URL + "/auth/reset-password",
-
 }
