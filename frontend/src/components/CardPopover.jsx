60e5e8791d3fd133e4ee50b7a48439f60cb34b82
hp0901 <hp98570018@gmail.com>
2026-01-25 20:19:45 +0530

    Aws 7

--- frontend/src/components/CardPopover.jsx
+++ frontend/src/components/CardPopover.jsx
@@ -7,13 +7,22 @@ const CardPopover = ({ cardData, close }) => {
       className="
         fixed inset-0 z-50
         flex justify-center items-start
+        lg:items-center
         md:absolute md:inset-auto md:right-14 md:top-14
       "
     >
       {/* click outside overlay */}
       <div className="fixed inset-0" onClick={close} />
 
-      <div className="relative bg-transparent rounded-xl shadow-xl p-2 w-full max-w-[380px] mt-16 md:mt-0">
+      <div
+        className="
+          relative bg-transparent rounded-xl shadow-xl p-2
+          w-full max-w-[380px] mt-16
+          lg:mt-0
+          lg:max-w-[520px]
+          lg:scale-105
+        "
+      >
         {cardData.isGuest ? (
           <GuestCard />
         ) : (
